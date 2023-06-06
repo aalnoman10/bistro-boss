4af3332dfaa47a85d38287e0834677c2f3fd751e
Al Noman <aalnomanm10@gmail.com>
2023-06-06 09:49:47 -0700

    my cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,11 +1,47 @@
-import { Outlet } from "react-router-dom";
-import SideBar from "../pages/Shared/SideBar/SideBar";
+import { NavLink, Outlet } from "react-router-dom";
+import { FaAd, FaBars, FaBook, FaHome, FaShopify, FaUser, FaVoicemail } from 'react-icons/fa'
 
 const Dashboard = () => {
+    const admin = false;
+
     return (
-        <div>
-            <SideBar />
-            <Outlet />
+        <div className="drawer drawer-mobile ">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content p-2 md:py-8 md:px-4 bg-slate-50">
+                {/* Page content here */}
+                <label htmlFor="my-drawer-2" className="btn btn-primary normal-case drawer-button lg:hidden">Sidebar</label>
+                <Outlet />
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className="custom-side-menu menu p-4 py-7 w-80 h-full bg-yellow-600 text-white">
+                    <div className="text-center mb-10">
+                        <h3 className="text-3xl uppercase font-semibold text-black">Bistro boss</h3>
+                        <h4 className="text-xl uppercase text-black font-semibold">R e s t a u r a n t</h4>
+                    </div>
+                    {/* Sidebar content here */}
+                    {admin ? <>
+                        <li><NavLink to='/dashboard/admin-home' className="uppercase"><FaHome size={24} />Admin Home</NavLink></li>
+                        <li><NavLink to='/dashboard/add-item' className="uppercase"><FaAd size={24} />add items</NavLink></li>
+                        <li><NavLink to='/dashboard/manage-item' className="uppercase"><FaBars size={24} />manage items</NavLink></li>
+                        <li><NavLink to='/dashboard/manage-bookig' className="uppercase"><FaBook size={24} />Manage bookings</NavLink></li>
+                        <li><NavLink to='/dashboard/all-user' className="uppercase"><FaUser size={24} />all users</NavLink></li>
+                    </> : <>
+                        <li><NavLink to='/dashboard/user-home' className="uppercase"><FaHome size={24} />User Home</NavLink></li>
+                        <li><NavLink to='/dashboard/reservation' className="uppercase"><FaAd size={24} />reservation</NavLink></li>
+                        <li><NavLink to='/dashboard/payment-history' className="uppercase"><FaBars size={24} />payment history</NavLink></li>
+                        <li><NavLink to='/dashboard/cart' className="uppercase"><FaBars size={24} />my cart</NavLink></li>
+                        <li><NavLink to='/dashboard/add-review' className="uppercase"><FaUser size={24} />add review</NavLink></li>
+                        <li><NavLink to='/dashboard/bookig' className="uppercase"><FaBook size={24} />my booking</NavLink></li>
+                    </>
+                    }
+                    <hr className="my-7" />
+                    <li><NavLink to='/' className="uppercase"><FaHome size={24} />Home</NavLink></li>
+                    <li><NavLink to='/menu' className="uppercase"><FaBars size={24} />Menu</NavLink></li>
+                    <li><NavLink to='/shop' className="uppercase"><FaShopify size={24} />Shop</NavLink></li>
+                    <li><NavLink to='/contact' className="uppercase"><FaVoicemail size={24} />Contact</NavLink></li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/pages/Daseboad/DashboardCarts/DashboardCarts.jsx
+++ src/pages/Daseboad/DashboardCarts/DashboardCarts.jsx
@@ -1,8 +1,51 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useCarts from "../../../hooks/useCarts";
 
 const DashboardCarts = () => {
+
+    const [, cart] = useCarts();
+
+    const sum = cart.reduce((pre, cur) => cur.price + pre, 0);
+
     return (
         <div>
-            DashboardCarts
+            <SectionTitle subHeading="urry Up!" heading={'MANAGE ALL ITEMS'} />
+            <div className="grid place-items-center">
+                <div className="bg-white p-8 rounded-lg">
+                    <div className="flex justify-between items-center mb-4">
+                        <h3 className="text-2xl font-semibold uppercase">total items : {cart.length}</h3>
+                        <div className="flex items-center space-x-2">
+                            <h4>total Price : $ {sum}</h4>
+                            <button className="btn bg-yellow-600 normal-case">pay</button>
+                        </div>
+                    </div>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr className="">
+                                    <th> # </th>
+                                    <th>Items Image</th>
+                                    <th>Items Name</th>
+                                    <th>Price</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* body  */}
+                                {cart.map((item, index) => <tr key={index}>
+                                    <th>{index + 1}</th>
+                                    <td><img className="w-16 h-16 rounded-lg" src={item?.image} alt="" /></td>
+                                    <td><h3 className="text-xl"> Name{item?.name}</h3></td>
+                                    <td><h6 className="text-end">$ {item?.price}</h6></td>
+                                    <td><button className="btn btn-ghost btn-xs">delete</button></td>
+                                </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
